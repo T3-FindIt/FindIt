1d162d1704d31e1382efb6ffa962948821d5cbc6
Verlinde,Tom T.S <t.verlinde@student.fontys.nl>
2023-02-08 16:28:59 +0100

    Test met python testing script

---
+++ StyleCheck.py
@@ -0,0 +1,11 @@
+import sys
+
+def printToStderr(string:str):
+    print(string, file=sys.stderr)
+
+def main():
+    printToStderr("This test should FAIL, and this string should be easily visible in the output.")
+    return -1
+
+if __name__ == "__main__":
+    main()
