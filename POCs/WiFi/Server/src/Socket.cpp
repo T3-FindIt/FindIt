9c373cc3999fee6a8a7e7a08516ad9b67276ffe0
Verlinde,Tom T.S <t.verlinde@student.fontys.nl>
2023-04-02 21:13:11 +0200

    Updated indentation & improved parameters

--- POCs/WiFi/Server/src/Socket.cpp
+++ POCs/WiFi/Server/src/Socket.cpp
@@ -1,96 +1,104 @@
 #include "Socket.hpp"
 #include <iostream>
-
-#define _WIN32_WINNT_WIN10                  0x0A00 // Windows 10
+#include <limits.h>
 
 namespace FindIt
 {
 
-Socket::Socket(std::string ipAddress, int port, MessageReceivedHandler handler)
-		: m_ipAddress(ipAddress), m_port(port), MessageReceived(handler)
+Socket::Socket(const std::string& ipAddress,
+                int port,
+                MessageReceivedHandler handler)
+        : m_ipAddress(ipAddress),
+        m_port(port),
+        m_messageReceived(handler)
 {
 }
 
-void Socket::Send(int clientSocket, std::string message)
+void Socket::Send(int clientSocket, const std::string& message)
 {
-	send(clientSocket, message.c_str(), message.size() + 1, 0);
+    std::size_t messageLen = message.size() + 1;
+    
+    if (messageLen <= INT_MAX)
+    {
+        send(clientSocket, message.c_str(), (int)messageLen, 0);
+    }
 }
 
 bool Socket::Init()
 {
-	WSAData wsaData;
-	int wsInit = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	//TODO: error handling
-	return wsInit == 0;
+    WSAData wsaData;
+    int wsInit = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    //TODO: error handling
+    return wsInit == 0;
 }
 
 [[noreturn]] void Socket::Run()
 {
-	char buf[4096];
+    char buf[4096];
 
-	while (true)
-	{
-		SOCKET listening = CreateSocket();
-		if (listening == INVALID_SOCKET)
-		{
-			break;
-		}
+    while (true)
+    {
+        SOCKET listening = CreateSocket();
+        if (listening == INVALID_SOCKET)
+        {
+            break;
+        }
 
-		SOCKET client = WaitForConnection(listening);
-		if (client != INVALID_SOCKET)
-		{
-			closesocket(listening);
+        SOCKET client = WaitForConnection(listening);
+        if (client != INVALID_SOCKET)
+        {
+            closesocket(listening);
 
-			int bytesReceived = 0;
-			do
-			{
-				ZeroMemory(buf, 4096);
-				bytesReceived = recv(client, buf, 4096, 0);
-				if (bytesReceived > 0 && MessageReceived != NULL)
-				{
-                    MessageReceived(this, client, std::string(buf, 0, bytesReceived));
-				}
-			} while (bytesReceived > 0);
-			closesocket(client);
-		}
-	}
+            int bytesReceived = 0;
+            do
+            {
+                ZeroMemory(buf, 4096);
+                bytesReceived = recv(client, buf, 4096, 0);
+                if (bytesReceived > 0 && m_messageReceived != nullptr)
+                {
+                    m_messageReceived(this, client, std::string(buf, 0, bytesReceived));
+                }
+            } while (bytesReceived > 0);
+            closesocket(client);
+        }
+    }
 }
 
 Socket::~Socket()
 {
-	WSACleanup();
+    WSACleanup();
 }
 
 SOCKET Socket::CreateSocket()
 {
-	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
-	if (listening != INVALID_SOCKET)
-	{
-		sockaddr_in hint;
-		hint.sin_family = AF_INET;
-		hint.sin_port = htons(m_port);
+    SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+    if (listening != INVALID_SOCKET)
+    {
+        sockaddr_in hint;
+        hint.sin_family = AF_INET;
+        hint.sin_port = htons(m_port);
 
-		hint.sin_addr.S_un.S_addr = inet_addr(m_ipAddress.c_str());
-		//hint.sin_addr.S_un.S_addr = INADDR_ANY;
+        hint.sin_addr.S_un.S_addr = inet_addr(m_ipAddress.c_str());
+        // hint.sin_addr.S_un.S_addr = INADDR_ANY;
 
-		int bindOk = bind(listening, (sockaddr *) &hint, sizeof(hint));
-		if (bindOk != SOCKET_ERROR)
-		{
-			int listenOk = listen(listening, SOMAXCONN);
-			if (listenOk != SOCKET_ERROR)
-			{
-				return listening;
-			}
-		}
-	}
-	return INVALID_SOCKET;
-	//return -1;
+        int bindOk = bind(listening, (sockaddr *) &hint, sizeof(hint));
+        if (bindOk != SOCKET_ERROR)
+        {
+            int listenOk = listen(listening, SOMAXCONN);
+            if (listenOk != SOCKET_ERROR)
+            {
+                return listening;
+            }
+        }
+    }
+    return INVALID_SOCKET;
+    //return -1;
 }
 
 SOCKET Socket::WaitForConnection(SOCKET listening)
 {
-	SOCKET client = accept(listening, nullptr, nullptr);
-	return client;
+    SOCKET client = accept(listening, nullptr, nullptr);
+    return client;
 }
 
 } // namespace FindIt
--- POCs/WiFi/Server/src/Socket.hpp
+++ POCs/WiFi/Server/src/Socket.hpp
@@ -1,7 +1,6 @@
 #ifndef SOCKET_HPP
 #define SOCKET_HPP
 
-#include <iostream>
 #include <ws2tcpip.h>
 #include <winsock2.h>
 #include <string>
@@ -11,21 +10,21 @@ namespace FindIt
 
 class Socket;
 
-// typedef void (*MessageReceivedHandler)(TCPListener *listener, int socketId, std::string message);
 using MessageReceivedHandler = void (*)(Socket *listener, int socketId, std::string message);
 
 class Socket
 {
 private:
-    SOCKET CreateSocket();
-	SOCKET WaitForConnection(SOCKET listening);
 	std::string m_ipAddress;
 	int m_port;
-	MessageReceivedHandler MessageReceived;
+	MessageReceivedHandler m_messageReceived;
+    
+	SOCKET CreateSocket();
+	SOCKET WaitForConnection(SOCKET listening);
 public:
-    Socket(std::string ipAddress, int port, MessageReceivedHandler handler);
+    Socket(const std::string& ipAddress, int port, MessageReceivedHandler handler);
 	~Socket();
-	void Send(int clientSocket, std::string message);
+	void Send(int clientSocket, const std::string& message);
 	bool Init();
 	[[noreturn]] void Run();
 };
