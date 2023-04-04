2419652be4e7483cbf300f7791ac62b7672f4c4d
Verlinde,Tom T.S <t.verlinde@student.fontys.nl>
2023-04-04 12:36:20 +0200

    Added C pointer ref fix

--- StyleChecker/modules/C/Checker.py
+++ StyleChecker/modules/C/Checker.py
@@ -155,7 +155,7 @@ def CheckForOperator(line:str, index:int, currentFilePath:str):
 
     # Regex to check for correct operators.
     # singleRegex = '(\+|-|=|\/|\*|%|==|!=|>|<|>=|<=|&&|\|\||&|\||\^|~|\+=|-=|\*=|\/=|%=|<<=|>>=|&=|\^=|\|=|<<|>>)'
-    singleRegex = '(\+|-|=|\/|%|==|!=|>|<|>=|<=|&&|\|\||\||\^|~|\+=|-=|\*=|\/=|%=|<<=|>>=|&=|\^=|\|=|<<|>>)'
+    singleRegex = '(?(?!=(->))(\+|-|=|\/|%|==|!=|>|<|>=|<=|&&|\|\||\||\^|\+=|-=|\*=|\/=|%=|<<=|>>=|&=|\^=|\|=|<<|>>))'
     completeRegex = '(\\b' + singleRegex + '\\b)|(' + singleRegex + '\\b)|(\\b' + singleRegex + ')'
     if re.search(completeRegex, line) is not None:
         PrintStyleError(index, line, currentFilePath, 'Incorrect operator.', 'Operators must be preceded and followed by a whitespace.')
