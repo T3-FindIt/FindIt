8eeb254f110ca0b33e758fbf8465dcdcb409e80c
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-04-04 14:45:27 +0200

    Create IServerHandler.hpp
    Adheres to coding standard & class diagram

---
+++ Source/Master/src/IServerHandler.hpp
@@ -0,0 +1,17 @@
+#ifndef ISERVERHANDLER_HPP
+#define ISERVERHANDLER_HPP
+
+#include <string>
+
+class IServerHandler
+{
+    public:
+    virtual void Send(std::string json) = 0;
+    virtual std::string Recieve() = 0;
+
+    virtual bool Connect() = 0;
+    virtual void Disconnect() = 0;
+    virtual bool isConnected() = 0;
+};
+
+#endif
