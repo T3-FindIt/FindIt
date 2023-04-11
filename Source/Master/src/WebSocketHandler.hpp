d56a7a08d3343e1a4de17737d7e0d5e185135ce8
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-04-11 09:34:54 +0200

    Implemented WebSocketHandler

--- Source/Master/src/WebSocketHandler.hpp
+++ Source/Master/src/WebSocketHandler.hpp
@@ -9,6 +9,12 @@ struct WebSocketData
     std::string IP;
     int Port;
 
+    WebSocketData()
+    {
+        IP = "";
+        Port = 0;
+    }
+
     WebSocketData(std::string ip, int port)
     {
         IP = ip;
@@ -30,8 +36,9 @@ struct WebSocketData
 class WebSocketHandler : public IServerHandler
 {
     private:
+    WebSocketData data;
     public:
-    WebSocketHandler(int port);
+    WebSocketHandler(WebSocketData& data);
     void Send(std::string json);
     std::string Recieve();
 
