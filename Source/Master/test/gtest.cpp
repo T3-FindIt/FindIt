09966de40fa9ed0965c42ee8f858ace515abd8a8
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-04-18 14:11:34 +0200

    Attempting to fix testing

--- Source/Master/test/gtest.cpp
+++ Source/Master/test/gtest.cpp
@@ -1,14 +1,81 @@
 #include <gtest/gtest.h>
 
-TEST(DummyTest, ShouldPass)
+#include "./JsonBuilder.hpp"
+
+TEST(JsonBuilderTest, Test_Deserialize_Correct_Message)
 {
-    EXPECT_EQ(1, 1);
+    JsonBuilder jsonBuilder = JsonBuilder();
+    std::string json = "{\"Action\":\"HeartBeat\"}";
+    EXPECT_TRUE(jsonBuilder.Deserialize(json));
 }
 
-TEST(DummyTest, ShouldFail)
+TEST(JsonBuilderTest, Test_Deserialize_Incorrect_Payload)
 {
-    EXPECT_EQ(1, 2);
+    JsonBuilder jsonBuilder = JsonBuilder();
+    std::string json = "{\"Action\":\"HeatBeater\"}";
+    EXPECT_FALSE(jsonBuilder.Deserialize(json));
 }
+
+TEST(JsonBuilderTest, Test_Deserialize_Incorrect_Header)
+{
+    JsonBuilder jsonBuilder = JsonBuilder();
+    std::string json = "{\"Act1on\":\"HeartBeat\"}";
+    EXPECT_FALSE(jsonBuilder.Deserialize(json));
+}
+
+TEST(JsonBuilderTest, Test_Deserialize_Empty_Message)
+{
+    JsonBuilder jsonBuilder = JsonBuilder();
+    std::string json = "";
+    EXPECT_FALSE(jsonBuilder.Deserialize(json));
+}
+
+TEST(JsonBuilderTest, Test_Serialize_NULL_Action)
+{
+    JsonBuilder jsonBuilder = JsonBuilder();
+    int data[1] = {0};
+    std::string output = "";
+    EXPECT_FALSE(jsonBuilder.Serialize(NULL, data, 1, output));
+}
+
+TEST(JsonBuilderTest, Test_Serialize_NULL_Data)
+{
+    JsonBuilder jsonBuilder = JsonBuilder();
+    std::string actions[1] = {"HeartBeat"};
+    std::string output = "";
+    EXPECT_FALSE(jsonBuilder.Serialize(actions, NULL, 1, output));
+}
+
+TEST(JsonBuilderTest, Test_Serialize_Zero_Size)
+{
+    JsonBuilder jsonBuilder = JsonBuilder();
+    std::string actions[1] = {"HeartBeat"};
+    int data[1] = {0};
+    std::string output = "";
+    EXPECT_FALSE(jsonBuilder.Serialize(actions, data, 0, output));
+    EXPECT_FALSE(jsonBuilder.Serialize(actions, data, -1, output));
+}
+
+TEST(JsonBuilderTest, Test_Serialize_Correct_Message)
+{
+    JsonBuilder jsonBuilder = JsonBuilder();
+    std::string actions[1] = {"HeartBeat"};
+    int data[1] = {0};
+    std::string output = "";
+    EXPECT_TRUE(jsonBuilder.Serialize(actions, data, 1, output));
+    EXPECT_EQ(output, "{\"Action\":\"HeartBeat\",\"Data\":0}");
+}
+
+
+// TEST(DummyTest, ShouldPass)
+// {
+//     EXPECT_EQ(1, 1);
+// }
+
+// TEST(DummyTest, ShouldFail)
+// {
+//     EXPECT_EQ(1, 2);
+// }
 
 #if defined(ARDUINO)
 #include <Arduino.h>
