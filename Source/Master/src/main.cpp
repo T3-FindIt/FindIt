e2e22f94933f360d847400cf5f1367573faab064
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-04-18 10:13:39 +0200

    Finalized Setup function

--- Source/Master/src/main.cpp
+++ Source/Master/src/main.cpp
@@ -4,8 +4,35 @@
 #include "WiFiHandler.hpp"
 #include "WebSocketHandler.hpp"
 
+#define LOCAL_ADDRESS 0x1
+
+// Might be better to put this in a secret.h file
+#define SSID "SSID"
+#define PASSWORD "PASSWORD"
+
+#define SERVER_ADDRESS "SERVER IP"
+#define SERVER_PORT 80
+
 void setup() {
-  // put your setup code here, to run once:
+  WiFiData wifiData(SSID, PASSWORD);
+  WiFiHandler wifiHandler(wifiData);
+
+  WebSocketData webSocketData(SERVER_ADDRESS, SERVER_PORT);
+  WebSocketHandler webSocketHandler(webSocketData);
+
+  wifiHandler.Connect();
+  if(!wifiHandler.isConnected())
+  {
+    return;
+  }
+
+  webSocketHandler.Connect();
+  if(!webSocketHandler.isConnected())
+  {
+    return;
+  }
+
+  I2C i2c(0x1);
 }
 
 void loop() {
