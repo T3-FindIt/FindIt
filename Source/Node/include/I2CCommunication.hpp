6f860ce083e83f1a96627cdb359616d3d9ffead5
coen hezemans <i481948@fhict.nl>
2023-04-18 14:41:16 +0200

    setupI2Ccom voor arduino, verwacht klaar before testing fase

--- Source/Node/include/I2CCommunication.hpp
+++ Source/Node/include/I2CCommunication.hpp
@@ -10,23 +10,20 @@
 #define ITEM_REG 0x03
 #define ACTIVE_REG 0x04
 #define ERROR_REG 0x05
-#define SETUP_REG 0x06
+
+#define HUB_ADDRESS 0x08
+#define NODE_ADRESS 0x7F
 
 class I2CCommunication
 {
 private:
-    const int DefaultAdress = 0x7f;
-    //uint8_t systemState; //0 is idle, 1 is sending, 2 is recieving //not needed for now
-    uint8_t RGBValues[3];
-    bool setupState; //0 is idle, 1 is set up for messaging
-    uint8_t NotificationRegistryValues;
-    uint8_t activeRegister;
-    static void OnRecieve(int HowMany);
-    static void OnRequest();
+
 public:
     I2CCommunication();
     ~I2CCommunication();
     int SendNewItemToHub(char* itemstring);
-    uint8_t GetRGB();
-    uint8_t GetNotificationSettings();
+    void GetRGBValues(uint8_t* OutputArray);
+    uint8_t GetNotificationModeRegistry();
+    uint8_t GetNotificationState();
+    uint8_t GetRecievedErrorState();
 };
--- Source/Node/lib/I2CCommunication.cpp
+++ Source/Node/lib/I2CCommunication.cpp
@@ -1,30 +1,60 @@
 #include "I2CCommunication.hpp"
 
-void I2CCommunication::OnRecieve(int HowMany)
+volatile uint8_t activeRegisterAdress = 0;
+volatile uint8_t Item[48];
+volatile uint8_t RGBValues[3];
+volatile uint8_t notificationModeRegistry = 0;
+volatile bool notificationState = 0;
+volatile uint8_t recievedErrorState = 0;
+volatile uint8_t errorState = 0;
+
+void OnRecieve(int HowMany)
 {
-    switch (activeRegister)
+    switch (activeRegisterAdress)
     {
     case REQUESTFROM_REG:
-        break;
-    case NOTIFICATION_REG:
-        break;
-    case RGB_REG:
-        break;
-    case ITEM_REG:
-        break;
-    case ERROR_REG:
-        break;
-    case SETUP_REG:
-        break;
-    
-    default:
+    {
+        //requestfrom shizzle :)
+        Wire.flush();
         break;
     }
+    case NOTIFICATION_REG:
+    {
+        notificationModeRegistry = Wire.read();
+        break;
+    }
+    case RGB_REG:
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            RGBValues[i] = Wire.read();
+        }
+        break;
+    }
+    case ACTIVE_REG:
+    {
+        notificationState = Wire.read();
+        break;
+    }
+    case ERROR_REG:
+    {
+        recievedErrorState = Wire.read();
+        break;
+    }
+    default:
+    {
+        activeRegisterAdress = Wire.read();
+        break;
+    }
+    }
+    Wire.flush();
 }
 
-void I2CCommunication::OnRequest()
+void OnRequest()
 {
-
+    Wire.beginTransmission(HUB_ADDRESS);
+    Wire.write(errorState);
+    Wire.endTransmission();
 }
 
 I2CCommunication::I2CCommunication()
@@ -32,41 +62,42 @@ I2CCommunication::I2CCommunication()
     RGBValues[0] = 0;
     RGBValues[1] = 0;
     RGBValues[2] = 0;
-    setupState = false;
-    NotificationRegistryValues = 0;
-    activeRegister = SETUP_REG;
-    Wire.begin(DefaultAdress);
-    Wire.onReceive(OnRecieve);
-    Wire.onRequest(OnRequest);
-    int newAdress = 0;
-    Wire.requestFrom(0x08, 1); 
-    while (!Wire.available())
-    {
-        //do nothing
-    }
-    newAdress = Wire.read();
-    Wire.end();
-    Wire.begin(newAdress);
-    Wire.onReceive(OnRecieve);
-    Wire.onRequest(OnRequest);
+    notificationModeRegistry = 0;
+    notificationState = false;
+    activeRegisterAdress = 0xFF;
+    Wire.begin(NODE_ADRESS);
 }
 
 I2CCommunication::~I2CCommunication()
 {
-
+    Wire.end();
 }
 
-uint8_t I2CCommunication::GetNotificationSettings()
+int I2CCommunication::SendNewItemToHub(char* itemString)
 {
-
+    Wire.beginTransmission(HUB_ADDRESS);
+    Wire.write(ITEM_REG);
+    for(int i = 0; i < 48; i++)
+    {
+        Wire.write(itemString[i]);
+    }
+    Wire.endTransmission();
 }
 
-uint8_t I2CCommunication::GetRGB()
+void I2CCommunication::GetRGBValues(uint8_t* OutputArray)
 {
-
+    for(int i = 0 ; i < 3; i++)
+    {
+        OutputArray[i] = RGBValues[i];
+    }
 }
 
-int I2CCommunication::SendNewItemToHub(char* newItem)
+uint8_t I2CCommunication::GetNotificationModeRegistry()
 {
+    return notificationModeRegistry;
+}
 
+uint8_t I2CCommunication::GetRecievedErrorState()
+{
+    return recievedErrorState;
 }
