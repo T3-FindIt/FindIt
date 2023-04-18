953c574951a965614da71bfcf44ca23162d68da3
Yassine Tama <i415098@fhict.nl>
2023-04-18 15:33:11 +0200

    Update TCPClusterConnection.cpp
    functies in de klasse gemaakt.

--- Source/Server/src/Communication/ClusterConnection/TCPClusterConnection.cpp
+++ Source/Server/src/Communication/ClusterConnection/TCPClusterConnection.cpp
@@ -2,5 +2,28 @@
 
 namespace FindIt
 {
-    TCPClusterConnection::
-}
+    TCPClusterConnection::TCPClusterConnection()
+    {
+        //ctor
+    }
+
+    [[noreturn]] void TCPClusterConnection::Run()
+    {
+        //fillin
+    }
+
+    void TCPClusterConnection::Stop()
+    {
+        //fillin
+    }
+
+    void TCPClusterConnection::Broadcast(iMessage message)
+    {
+        //fillin
+    }
+
+    void TCPClusterConnection::SetOnMessagehandler(Event_function_t handler)
+    {
+        //fillin
+    }
+} // namespace FindIt
