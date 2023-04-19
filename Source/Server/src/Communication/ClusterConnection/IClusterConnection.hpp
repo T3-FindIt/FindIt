7a8dfe41e1d729c4eb450845b7caa4e302aaf900
Verlinde,Tom T.S <t.verlinde@student.fontys.nl>
2023-04-19 18:36:53 +0200

    IClusterConnection implemented

--- Source/Server/src/Communication/ClusterConnection/IClusterConnection.hpp
+++ Source/Server/src/Communication/ClusterConnection/IClusterConnection.hpp
@@ -3,20 +3,25 @@
 
 #include "../../Message/IMessage.hpp"
 
+#include <functional>
+
 namespace FindIt
 {
-    using Event_function_t = void();
-    
-    class IClusterConnection
-    {
-    private:
-        Event_function_t onMessageHandler;
-    public:
-        virtual [[noreturn]] void Run() = 0;
-        virtual void Stop() = 0;
-        virtual void Broadcast(iMessage message) = 0;
-        virtual void SetOnMessagehandler(Event_function_t handler) = 0;
-    };
+
+using Event_function_t = std::function<void()>;
+
+class IClusterConnection
+{
+private:
+    Event_function_t onMessageHandler;
+public:
+    virtual ~IClusterConnection() = default;
+    virtual void Run() = 0;
+    virtual void Stop() = 0;
+    virtual void Broadcast(IMessage& message) = 0;
+    virtual void SetOnMessageHandler(Event_function_t handler) = 0;
 };
 
-#endif //ICLUSTERCONNECTION_HPP
+};
+
+#endif // ICLUSTERCONNECTION_HPP
