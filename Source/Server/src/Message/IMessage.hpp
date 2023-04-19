23e8631eecf6bf8eea571093f94b20b9f9655c42
Verlinde,Tom T.S <t.verlinde@student.fontys.nl>
2023-04-19 18:39:08 +0200

    IMessage setup

---
+++ Source/Server/src/Message/IMessage.hpp
@@ -0,0 +1,30 @@
+#ifndef IMESSAGE_HPP
+#define IMESSAGE_HPP
+
+namespace FindIt
+{
+
+enum class MessageType
+{
+    SERVER_REQUEST_PRODUCT,
+    NODE_RESPOND_TO_PRODUCT_REQUEST,
+    NODE_EVENT_PRODUCT_FOUND,
+    NODE_SIGN_IN,
+    NODE_SIGN_IN_RESPONSE,
+    NODE_NOTIFY_NEW_PRODUCT,
+    NODE_NOTIFY_NEW_PRODUCT_RESPONSE,
+    HEARTBEAT,
+    HEARTBEAT_RESPONSE,
+};
+
+class IMessage
+{
+protected:
+    MessageType type;
+public:
+    virtual MessageType GetType() = 0;
+};
+
+};
+
+#endif // IMESSAGE_HPP
