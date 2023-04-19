69c00ef177aaad46cabc511269cb4696fc222575
Verlinde,Tom T.S <t.verlinde@student.fontys.nl>
2023-04-19 21:58:27 +0200

    NodeEventProductFound implementation

---
+++ Source/Server/src/Message/ClientToServerMessages/NodeEventProductFound.hpp
@@ -0,0 +1,26 @@
+#ifndef NODEEVENTPRODUCTFOUND_HPP
+#define NODEEVENTPRODUCTFOUND_HPP
+
+#include "../IMessage.hpp"
+
+namespace FindIt
+{
+
+class NodeEventProductFound : public IMessage
+{
+private:
+    std::string product;
+    bool result;
+public:
+    NodeEventProductFound(std::string product, bool result);
+    ~NodeEventProductFound() = default;
+
+    MessageType GetType() final;
+    std::string GetAction();
+    std::string GetProduct();
+    bool GetResult();
+};
+
+}
+
+#endif // NODEEVENTPRODUCTFOUND_HPP
