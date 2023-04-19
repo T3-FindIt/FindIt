9672bf8890a0e3b37a733144321c6f27364a7e84
Verlinde,Tom T.S <t.verlinde@student.fontys.nl>
2023-04-19 18:38:46 +0200

    JSONProtocolParser setup

---
+++ Source/Server/src/Communication/ProtocolParser/JSONProtocolParser.cpp
@@ -0,0 +1,23 @@
+#include "JSONProtocolParser.hpp"
+
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+#include "../../Message/ServerToClientMessages/HeartBeat.hpp"
+
+
+namespace FindIt
+{
+
+    IMessage* JSONProtocolParser::Parse(std::string data)
+    {
+        auto test = json::parse(data);
+        IMessage* message = new HeartBeat();
+        return message;
+    }
+
+    std::string JSONProtocolParser::Parse(IMessage &data)
+    {
+        return std::string();
+    }
+
+};
