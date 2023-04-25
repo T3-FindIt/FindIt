fbdce1da6c40f738b469a9562baaf18fa6918a4b
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-04-25 15:19:01 +0200

    Testing Complete
    Basic I2C testing completed.
    Will fix for the actual implementation.

--- Source/Master/src/main.cpp
+++ Source/Master/src/main.cpp
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 
 #include "I2C.hpp"
-#include "WiFiHandler.hpp"
-#include "WebSocketHandler.hpp"
+// #include "WiFiHandler.hpp"
+// #include "WebSocketHandler.hpp"
 
-#define LOCAL_ADDRESS 0x1
+#define LOCAL_ADDRESS 0x08
 
 // Might be better to put this in a secret.h file
 #define SSID "SSID"
@@ -13,59 +13,52 @@
 #define SERVER_ADDRESS "SERVER IP"
 #define SERVER_PORT 80
 
-WiFiHandler wifiHandler = WiFiHandler();
-WebSocketHandler webSocketHandler = WebSocketHandler();
-I2C i2c = I2C(LOCAL_ADDRESS);
+// WiFiHandler wifiHandler = WiFiHandler();
+// WebSocketHandler webSocketHandler = WebSocketHandler();
+I2C i2c;
 
 void setup() {
-  WiFiData wifiData(SSID, PASSWORD);
-  wifiHandler = WiFiHandler(wifiData);
+  // WiFiData wifiData(SSID, PASSWORD);
+  // wifiHandler = WiFiHandler(wifiData);
 
-  WebSocketData webSocketData(SERVER_ADDRESS, SERVER_PORT);
-  webSocketHandler = WebSocketHandler(webSocketData);
+  // WebSocketData webSocketData(SERVER_ADDRESS, SERVER_PORT);
+  // webSocketHandler = WebSocketHandler(webSocketData);
 
-  wifiHandler.Connect();
-  if(!wifiHandler.isConnected())
-  {
-    return;
-  }
+  // wifiHandler.Connect();
+  // if(!wifiHandler.isConnected())
+  // {
+  //   return;
+  // }
 
-  webSocketHandler.Connect();
-  if(!webSocketHandler.isConnected())
-  {
-    return;
-  }
+  // webSocketHandler.Connect();
+  // if(!webSocketHandler.isConnected())
+  // {
+  //   return;
+  // }
 
-  Serial.begin(9600);
-
+  // Serial.begin(9600);
+  // Serial.println("Starting up!");
+  i2c = I2C(LOCAL_ADDRESS);
 }
 
 Node_Registers lastRequest = NR_None;
 
 void loop()
 {
-  if(webSocketHandler.isConnected())
-  {
-    std::string websocketData = webSocketHandler.Recieve();
-    if(websocketData != "")
-    {
-      // Decompile with JSON parser
-      // Do some stuff with the data
+  // Turn this into a function?
+  // if(webSocketHandler.isConnected())
+  // {
+  //   std::string websocketData = webSocketHandler.Recieve();
+  //   if(websocketData != "")
+  //   {
+  //     // Decompile with JSON parser
+  //     // Do some stuff with the data
 
-      Serial.print("Websocket data: ");
-      Serial.println(websocketData.c_str());
-    }
-  }
+  //     Serial.print("Websocket data: ");
+  //     Serial.println(websocketData.c_str());
+  //   }
+  // }
 
-  if(lastRequest != NR_None)
-  {
-    Serial.println("Last request: " + lastRequest);
-  }
-  else
-  {
-    lastRequest = i2c.GetLastChange();
-  }
-
-  // DIPSWITCH
-
+  // i2c.Send(0x7F, NR_Notification, 0x5);
+  // delay(1000);
 }
