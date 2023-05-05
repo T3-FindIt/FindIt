8844f02d4ecf3c7aba13352d895329891d916ccb
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-05-05 13:06:26 +0200

    Renabled & fixed Wifi

--- Source/Master/src/main.cpp
+++ Source/Master/src/main.cpp
@@ -1,43 +1,43 @@
 #include <Arduino.h>
 
 #include "I2C.hpp"
-// #include "WiFiHandler.hpp"
-// #include "WebSocketHandler.hpp"
+#include "WiFiHandler.hpp"
+#include "WebSocketHandler.hpp"
 
 #define LOCAL_ADDRESS 0x08
 
-// Might be better to put this in a secret.h file
+// Might be better to put this in a secret.h file. To be discussed with the Git Master
 #define SSID "SSID"
 #define PASSWORD "PASSWORD"
 
 #define SERVER_ADDRESS "SERVER IP"
 #define SERVER_PORT 80
 
-// WiFiHandler wifiHandler = WiFiHandler();
-// WebSocketHandler webSocketHandler = WebSocketHandler();
+WiFiHandler wifiHandler = WiFiHandler();
+WebSocketHandler webSocketHandler = WebSocketHandler();
 I2C i2c;
 
 void setup() {
-  // WiFiData wifiData(SSID, PASSWORD);
-  // wifiHandler = WiFiHandler(wifiData);
+  WiFiData wifiData(SSID, PASSWORD);
+  wifiHandler = WiFiHandler(wifiData);
 
-  // WebSocketData webSocketData(SERVER_ADDRESS, SERVER_PORT);
-  // webSocketHandler = WebSocketHandler(webSocketData);
+  WebSocketData webSocketData(SERVER_ADDRESS, SERVER_PORT);
+  webSocketHandler = WebSocketHandler(webSocketData);
 
-  // wifiHandler.Connect();
-  // if(!wifiHandler.isConnected())
-  // {
-  //   return;
-  // }
+  wifiHandler.Connect();
+  if(!wifiHandler.isConnected())
+  {
+    return;
+  }
 
-  // webSocketHandler.Connect();
-  // if(!webSocketHandler.isConnected())
-  // {
-  //   return;
-  // }
+  webSocketHandler.Connect();
+  if(!webSocketHandler.isConnected())
+  {
+    return;
+  }
 
-  // Serial.begin(9600);
-  // Serial.println("Starting up!");
+  Serial.begin(9600);
+  Serial.println("Starting up!");
   i2c = I2C(LOCAL_ADDRESS);
 }
 
@@ -45,20 +45,44 @@ Node_Registers lastRequest = NR_None;
 
 void loop()
 {
-  // Turn this into a function?
-  // if(webSocketHandler.isConnected())
-  // {
-  //   std::string websocketData = webSocketHandler.Recieve();
-  //   if(websocketData != "")
-  //   {
-  //     // Decompile with JSON parser
-  //     // Do some stuff with the data
+  if(webSocketHandler.isConnected())
+  {
+    std::string websocketData = webSocketHandler.Recieve();
+    if(websocketData != "")
+    {
+      // Decompile with JSON parser
+      // Do some stuff with the data
 
-  //     Serial.print("Websocket data: ");
-  //     Serial.println(websocketData.c_str());
-  //   }
-  // }
+      Serial.print("Websocket data: ");
+      Serial.println(websocketData.c_str());
+    }
 
-  // i2c.Send(0x7F, NR_Notification, 0x5);
-  // delay(1000);
+
+    webSocketHandler.Send("Hello from ESP32!");
+  }
+
+  Node_Registers lastRegister = i2c.GetLastChange();
+  char data[MAX_STRING_SIZE];
+
+  switch (lastRegister)
+  {
+   case NR_Item:
+   {
+    i2c.GetRegister(NR_Item, data);
+    Serial.print("Item: ");
+    Serial.println(data);
+    break;
+   }
+   case NR_Error:
+   {
+    i2c.GetRegister(NR_Error, data);
+    Serial.print("Error: ");
+    Serial.println(data);
+    break;    
+   }
+  default:
+    break;
+  }
+
+  delay(1000);
 }
