aff2c0db2c61066ea8b86ed9ce365c75c039ac0b
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-05-09 11:11:20 +0200

    Json Related files

--- Source/Master/src/JsonBuilder.cpp
+++ Source/Master/src/JsonBuilder.cpp
@@ -1,33 +1,7 @@
-// #include "JsonBuilder.hpp"
-// #include "../lib/json.hpp"
+#include "JsonBuilder.hpp"
+#include "../lib/json.hpp"
 
-// JsonBuilder::JsonBuilder()
-// {
-// }
+JsonBuilder::JsonBuilder()
+{
+}
 
-// bool JsonBuilder::Deserialize(std::string Json)
-// {
-
-//     if(Json == "")
-//     {
-//         return false;
-//     }
-
-//     // if(strcmp(action.c_str(), "HeartBeat") != 0)
-//     // {
-//     //     return false;
-//     // }
-
-//     return true;
-
-// }
-
-// bool JsonBuilder::Serialize(std::string actions[], int data[], int size, std::string &output)
-// {
-//     if(actions == NULL || data == NULL || size <= 0)
-//     {
-//         return false;
-//     }
-
-//     return false;
-// }
--- Source/Master/src/JsonBuilder.hpp
+++ Source/Master/src/JsonBuilder.hpp
@@ -5,6 +5,19 @@ class JsonBuilder
 public:
     JsonBuilder();
 
-    // bool Deserialize(std::string Json);
-    // bool Serialize(std::string actions[], int data[], int size, std::string &output);
+    /// @brief Deserializes a JSON string into the given keys and values
+    /// @param Json The input JSON string
+    /// @param keys The keys to use
+    /// @param values The values to use
+    /// @param size The size of the keys and values arrays
+    /// @return True if the deserialization was successful, false otherwise
+    // bool Deserialize(std::string &Json, std::string keys[], int values[], int size);
+
+    /// @brief Serializes a JSON string from the given keys and values
+    /// @param keys The keys to use
+    /// @param values The values to use
+    /// @param size The size of the keys and values arrays
+    /// @param output The output string
+    /// @return True if the serialization was successful, false otherwise
+    // bool Serialize(std::string keys[], int data[], int size, std::string &output);
 };
