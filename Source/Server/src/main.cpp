e469f2f5b9e385fcb6a8f1ed518d172983b97880
Verlinde,Tom T.S <t.verlinde@student.fontys.nl>
2023-05-16 16:30:50 +0200

    End of day commit
    New Structure found, partially implemented

--- Source/Server/src/main.cpp
+++ Source/Server/src/main.cpp
@@ -1,31 +1,40 @@
-#include <nlohmann/json.hpp>
-#include <iostream>
-#include <thread>
+// #include <nlohmann/json.hpp>
+// #include <iostream>
+// #include <thread>
 
-#include <Communication.hpp>
-#include "./Communication/ClusterConnection/TCPConnection.hpp"
-#include "./Communication/ProtocolParser/JSONProtocolParser.hpp"
+// #include <Communication.hpp>
+// #include "./Communication/ClusterConnection/TCPConnection.hpp"
+// #include "./Communication/ProtocolParser/JSONProtocolParser.hpp"
+
+// int main()
+// {
+//     // Set cluster coms to a TCP connection on port 54000
+//     FindIt::Server* clusterConnection = new FindIt::Server(54000);
+
+//     // Set protocol parser to JSON
+//     FindIt::JSONProtocolParser* protocolParser = new FindIt::JSONProtocolParser();
+
+//     // Set up communication and start a new thread for it
+//     FindIt::Communication* communication = new FindIt::Communication(*clusterConnection, *protocolParser);
+//     std::jthread communicationThread(&FindIt::Communication::Run, communication);
+
+//     std::cin.get();
+
+//     communication->Stop();
+//     communicationThread.join();
+
+//     delete communication;
+//     delete protocolParser;
+//     delete clusterConnection;
+
+//     return 0;
+// }
+
+#include <Object.hpp>
+#include <PlainFileDatabase.hpp>
+#include <UserInterface.hpp>
 
 int main()
 {
-    // Set cluster coms to a TCP connection on port 54000
-    FindIt::Server* clusterConnection = new FindIt::Server(54000);
-
-    // Set protocol parser to JSON
-    FindIt::JSONProtocolParser* protocolParser = new FindIt::JSONProtocolParser();
-
-    // Set up communication and start a new thread for it
-    FindIt::Communication* communication = new FindIt::Communication(*clusterConnection, *protocolParser);
-    std::jthread communicationThread(&FindIt::Communication::Run, communication);
-
-    std::cin.get();
-
-    communication->Stop();
-    communicationThread.join();
-
-    delete communication;
-    delete protocolParser;
-    delete clusterConnection;
-
     return 0;
 }
