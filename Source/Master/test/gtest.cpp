a1be1d0dd058722ea246debd7d2e2f1cfe5fc090
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-05-16 14:37:04 +0200

    Implemented testing

--- Source/Master/test/gtest.cpp
+++ Source/Master/test/gtest.cpp
@@ -1,81 +1,101 @@
 #include <gtest/gtest.h>
-
 #include "JsonBuilder.hpp"
 
-// TEST(JsonBuilderTest, Test_Deserialize_Correct_Message)
-// {
-//     JsonBuilder jsonBuilder = JsonBuilder();
-//     std::string json = "{\"Action\":\"HeartBeat\"}";
-//     EXPECT_TRUE(jsonBuilder.Deserialize(json));
-// }
+TEST(JsonBuilderTest, Test_Deserialize_Correct_Message)
+{
+    JsonBuilder jsonBuilder;
+    std::string json = "{\"Action\":\"HeartBeat\"}";
+    std::string keys[] = {""};
+    std::string values[] = {""};
+    int length = 1;
+    
+    EXPECT_TRUE(jsonBuilder.Deserialize(json, keys, values, length));
+    EXPECT_EQ("Action", keys[0]);
+    EXPECT_EQ("HeartBeat", values[0]);
+}
 
-// TEST(JsonBuilderTest, Test_Deserialize_Incorrect_Payload)
-// {
-//     JsonBuilder jsonBuilder = JsonBuilder();
-//     std::string json = "{\"Action\":\"HeatBeater\"}";
-//     EXPECT_FALSE(jsonBuilder.Deserialize(json));
-// }
+TEST(JsonBuilderTest, Test_Deserialize_Empty_Message)
+{
+    JsonBuilder jsonBuilder;
+    std::string json = "";
+    std::string keys[] = {""};
+    std::string values[] = {""};
+    int length = 1;
+    
+    EXPECT_FALSE(jsonBuilder.Deserialize(json, keys, values, length));
+}
 
-// TEST(JsonBuilderTest, Test_Deserialize_Incorrect_Header)
-// {
-//     JsonBuilder jsonBuilder = JsonBuilder();
-//     std::string json = "{\"Act1on\":\"HeartBeat\"}";
-//     EXPECT_FALSE(jsonBuilder.Deserialize(json));
-// }
+TEST(JsonBuilderTest, Test_Deserialize_Zero_Size)
+{
+    JsonBuilder jsonBuilder;
+    std::string json = "{\"Action\":\"HeartBeat\"}";
+    std::string keys[] = { "" };
+    std::string values[] = { "" };
+    int length = 0;
 
-// TEST(JsonBuilderTest, Test_Deserialize_Empty_Message)
-// {
-//     JsonBuilder jsonBuilder = JsonBuilder();
-//     std::string json = "";
-//     EXPECT_FALSE(jsonBuilder.Deserialize(json));
-// }
+    EXPECT_FALSE(jsonBuilder.Deserialize(json, keys, values, length));
+}
 
-// TEST(JsonBuilderTest, Test_Serialize_NULL_Action)
-// {
-//     JsonBuilder jsonBuilder = JsonBuilder();
-//     int data[1] = {0};
-//     std::string output = "";
-//     EXPECT_FALSE(jsonBuilder.Serialize(NULL, data, 1, output));
-// }
+TEST(JsonBuilderTest, Test_Deserialize_NULL_Keys)
+{
+    JsonBuilder jsonBuilder;
+    std::string json = "{\"Action\":\"HeartBeat\"}";
+    std::string values[] = {""};
+    int length = 1;
+    
+    EXPECT_FALSE(jsonBuilder.Deserialize(json, nullptr, values, length));
+}
 
-// TEST(JsonBuilderTest, Test_Serialize_NULL_Data)
-// {
-//     JsonBuilder jsonBuilder = JsonBuilder();
-//     std::string actions[1] = {"HeartBeat"};
-//     std::string output = "";
-//     EXPECT_FALSE(jsonBuilder.Serialize(actions, NULL, 1, output));
-// }
+TEST(JsonBuilderTest, Test_Deserialize_NULL_Values)
+{
+    JsonBuilder jsonBuilder;
+    std::string json = "{\"Action\":\"HeartBeat\"}";
+    std::string keys[] = {""};
+    int length = 1;
+    
+    EXPECT_FALSE(jsonBuilder.Deserialize(json, keys, nullptr, length));
+}
 
-// TEST(JsonBuilderTest, Test_Serialize_Zero_Size)
-// {
-//     JsonBuilder jsonBuilder = JsonBuilder();
-//     std::string actions[1] = {"HeartBeat"};
-//     int data[1] = {0};
-//     std::string output = "";
-//     EXPECT_FALSE(jsonBuilder.Serialize(actions, data, 0, output));
-//     EXPECT_FALSE(jsonBuilder.Serialize(actions, data, -1, output));
-// }
+TEST(JsonBuilderTest, Test_Serialize_NULL_Action)
+{
+    JsonBuilder jsonBuilder;
+    std::string json = "";
+    std::string values[] = {""};
+    int length = 1;
+    
+    EXPECT_FALSE(jsonBuilder.Deserialize(json, nullptr, values, length));
+}
 
-// TEST(JsonBuilderTest, Test_Serialize_Correct_Message)
-// {
-//     JsonBuilder jsonBuilder = JsonBuilder();
-//     std::string actions[1] = {"HeartBeat"};
-//     int data[1] = {0};
-//     std::string output = "";
-//     EXPECT_TRUE(jsonBuilder.Serialize(actions, data, 1, output));
-//     EXPECT_EQ(output, "{\"Action\":\"HeartBeat\",\"Data\":0}");
-// }
+TEST(JsonBuilderTest, Test_Serialize_NULL_Data)
+{
+    JsonBuilder jsonBuilder;
+    std::string json = "";
+    std::string actions[1] = {"HeartBeat"};
+    std::string output = "";
+    EXPECT_FALSE(jsonBuilder.Serialize(actions, NULL, 1, output));
+}
 
+TEST(JsonBuilderTest, Test_Serialize_Zero_Size)
+{
+    JsonBuilder jsonBuilder;
+    std::string actions[1] = {"HeartBeat"};
+    std::string data[] = {"0"};
+    std::string output = "";
+    EXPECT_FALSE(jsonBuilder.Serialize(actions, data, 0, output));
+    EXPECT_FALSE(jsonBuilder.Serialize(actions, data, -1, output));
+}
 
-// TEST(DummyTest, ShouldPass)
-// {
-//     EXPECT_EQ(1, 1);
-// }
+TEST(JsonBuilderTest, Test_Serialize_Correct_Message)
+{
+    JsonBuilder jsonBuilder;
+    std::string json = "";
+    std::string keys[] = {"Action", "Data"};
+    std::string data[] = {"HeartBeat", "0"};
+    int size = 2;
+    EXPECT_TRUE(jsonBuilder.Serialize(keys, data, size, json));
+    EXPECT_EQ(json, "{\"Action\":\"HeartBeat\",\"Data\":\"0\"}");
+}
 
-// TEST(DummyTest, ShouldFail)
-// {
-//     EXPECT_EQ(1, 2);
-// }
 
 #if defined(ARDUINO)
 #include <Arduino.h>
@@ -102,6 +122,7 @@ void loop()
 #else
 int main(int argc, char **argv)
 {
+    JsonBuilder builder;
     ::testing::InitGoogleTest(&argc, argv);
     if (RUN_ALL_TESTS())
     ;
