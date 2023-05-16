ab34c85a4bdedcd5ccc61a1b53c1c5bab9344b90
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-05-16 14:36:04 +0200

    Moved to library folder

---
+++ Source/Master/lib/JsonBuilder/JsonBuilder.cpp
@@ -0,0 +1,53 @@
+#include "JsonBuilder.hpp"
+#include "json.hpp"
+using json = nlohmann::json;
+
+JsonBuilder::JsonBuilder()
+{
+}
+
+bool JsonBuilder::Deserialize(std::string Json, std::string keys[], std::string values[], int size)
+{
+    if (keys == nullptr || values == nullptr)
+    {
+        return false;
+    }
+    if (size <= 0)
+    {
+        return false;
+    }
+
+    if(Json.empty())
+    {
+        return false;
+    }
+
+    json deserializedJson = json::parse(Json);
+
+
+
+    return true;
+}
+
+bool JsonBuilder::Serialize(std::string keys[], std::string values[], int size, std::string &output)
+{
+    if (keys == nullptr || values == nullptr)
+    {
+        return false;
+    }
+    if (size <= 0)
+    {
+        return false;
+    }
+
+    json serializedJson;
+
+    for (size_t i = 0; i < size; i++)
+    {
+        serializedJson[keys[i]] = values[i];
+    }
+    
+    output = serializedJson.dump(); // Convert to string
+
+    return true;
+}
--- Source/Master/lib/JsonBuilder/JsonBuilder.hpp
+++ Source/Master/lib/JsonBuilder/JsonBuilder.hpp
@@ -1,8 +1,7 @@
 #ifndef JSONBUILDER_HPP
 #define JSONBUILDER_HPP
 
-
-#include <string.h>
+#include <string>
 
 class JsonBuilder
 {
@@ -15,7 +14,7 @@ public:
     /// @param values The values to use
     /// @param size The size of the keys and values arrays
     /// @return True if the deserialization was successful, false otherwise
-    // bool Deserialize(std::string &Json, std::string keys[], int values[], int size);
+    bool Deserialize(std::string Json, std::string keys[], std::string values[], int size);
 
     /// @brief Serializes a JSON string from the given keys and values
     /// @param keys The keys to use
@@ -23,7 +22,7 @@ public:
     /// @param size The size of the keys and values arrays
     /// @param output The output string
     /// @return True if the serialization was successful, false otherwise
-    // bool Serialize(std::string keys[], int data[], int size, std::string &output);
+    bool Serialize(std::string keys[], std::string values[], int size, std::string &output);
 };
 
 #endif // JSONBUILDER_HPP
