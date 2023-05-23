b2dca94cd01916c602693c07ac16d19e2f851285
Verlinde,Tom T.S <t.verlinde@student.fontys.nl>
2023-05-23 21:29:34 +0200

    added ParseSignInToObject test

--- Source/Server/test/JSON/JSONTest.cpp
+++ Source/Server/test/JSON/JSONTest.cpp
@@ -21,4 +21,17 @@ TEST_F(JSONTests, ParseHeartBeatToObject)
     delete message;
 }
 
+TEST_F(JSONTests, ParseSignInToObject)
+{
+    std::string data = "{\"action\":\"SignIn\",\"Node\":\"NodeName\",\"Places\":3}";
+    FindIt::IMessage* message = parser.Parse(data);
+    ASSERT_NE(message, nullptr);
+    ASSERT_EQ(message->GetType(), FindIt::MessageType::NODE_SIGN_IN);
+    FindIt::NodeSignIn* signIn = dynamic_cast<FindIt::NodeSignIn*>(message);
+    ASSERT_EQ(signIn->GetAction(), "SignIn");
+    ASSERT_EQ(signIn->GetNode(), "NodeName");
+    ASSERT_EQ(signIn->GetPlaces(), 3);
+    delete message;
+}
+
 } // namespace
