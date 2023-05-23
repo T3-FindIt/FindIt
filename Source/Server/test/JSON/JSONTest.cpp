b9b0be0cf560634f8b253f36008ee4ffe1f2f6af
Verlinde,Tom T.S <t.verlinde@student.fontys.nl>
2023-05-23 21:29:27 +0200

    added ParseHeartBeatToObject test

--- Source/Server/test/JSON/JSONTest.cpp
+++ Source/Server/test/JSON/JSONTest.cpp
@@ -10,4 +10,15 @@ protected:
     FindIt::JSONProtocolParser parser;
 };
 
+TEST_F(JSONTests, ParseHeartBeatToObject)
+{
+    std::string data = "{\"action\":\"HeartBeat\"}";
+    FindIt::IMessage* message = parser.Parse(data);
+    ASSERT_NE(message, nullptr);
+    ASSERT_EQ(message->GetType(), FindIt::MessageType::HEARTBEAT);
+    FindIt::HeartBeat* heartbeat = dynamic_cast<FindIt::HeartBeat*>(message);
+    ASSERT_EQ(heartbeat->GetAction(), "HeartBeat");
+    delete message;
+}
+
 } // namespace
