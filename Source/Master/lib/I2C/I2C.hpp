dec74ca9bac7badd237e31cc9838e88cc6f3f562
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-05-24 14:08:04 +0200

    Update I2C.hpp
    Changed scan function, can now just be called.
    Changed define to match I2C behaviour
    Changed Constructor to improve code.

--- Source/Master/lib/I2C/I2C.hpp
+++ Source/Master/lib/I2C/I2C.hpp
@@ -4,17 +4,17 @@
 #include <Wire.h>
 #include "IMasterSlave.hpp"
 
-#define MAX_STRING_SIZE 48
+#define MAX_STRING_SIZE 30
 
 class I2C : public IMasterSlave
 {
     private:
     int address;
     public:
-    I2C();
+    I2C() = default;
     I2C(int address);
     void Send(int address,Node_Registers register, int data);
-    void Scan(int* addresses, int size);
+    void Scan();
     int GetRegister(Node_Registers my_register, void* data);
     Node_Registers GetLastChange();
 };
