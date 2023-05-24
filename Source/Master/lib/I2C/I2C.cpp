42b0fdbe8c82021c6d876b3915f210d024795751
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-05-24 14:08:15 +0200

    Reworked Scanning

--- Source/Master/lib/I2C/I2C.cpp
+++ Source/Master/lib/I2C/I2C.cpp
@@ -1,7 +1,9 @@
 #include "I2C.hpp"
-
-#include <Arduino.h>
 #include <string>
+
+#define MAX_I2C_ADDRESSES 127
+#define I2C_TIMEOUT 1000
+#define MILLIS_TO_SECONDS * 1000
 
 // ===================== //
 // ===== REGISTER ===== //
@@ -19,47 +21,25 @@ Node_Registers lastRecieved = Node_Registers::NR_None;
 
 void receiveEvent(int howMany) // A node sends data, not making a request.
 {
-    std::string data = "";
-    char inbound = ' ';
-    int reg = Wire.read();
-
-    while (Wire.available())
+    if (howMany == 1)
     {
-        inbound = Wire.read();
-        data += inbound;
+
     }
-
-    switch ((Node_Registers)reg)
-    {
-        case Node_Registers::NR_Item:
-        {
-            Item = data;
-            break;
-        }
-        case Node_Registers::NR_Error:
-        {
-            Error = data;
-            break;
-        }
-        default:
-        {
-            throw "Invalid Register!"; // This should never happen
-        }
-    }
-
-    lastRecieved = (Node_Registers)reg;
-
 }
 
-I2C::I2C()
+void RequestEvent()
 {
+    Wire.write(GetAvailableAddress());
 }
 
 I2C::I2C(int address)
 {
+    InitializeAddresses();
     this->address = address;
     Wire.begin(address);
     Wire.onReceive(receiveEvent);
+    Wire.onRequest(RequestEvent);
+    Wire.setTimeOut(I2C_TIMEOUT MILLIS_TO_SECONDS);
 
     // Serial.begin(9600); !Debug!
 }
@@ -78,21 +58,17 @@ void I2C::Send(int address,Node_Registers nodeRegister, int data)
     Wire.begin(this->address);
 }
 
-void I2C::Scan(int* addresses, int size)
+void I2C::Scan()
 {
-    int address = 0;
-    int index = 0;
-    for (address = 1; address < 127; address++)
+    for (int i = 0; i < 127; i++)
     {
-        Wire.beginTransmission(address);
-        if (Wire.endTransmission() == 0)
+        Wire.beginTransmission(i);
+        Wire.write((int)Node_Registers::NR_RequestForm);
+        Wire.endTransmission();
+        int heartbeat = Wire.requestFrom(i, 1);
+        if(heartbeat == 0)
         {
-            addresses[index] = address;
-            index++;
-            if (index >= size)
-            {
-                break;
-            }
+            ReleaseAddress(i);
         }
     }
 }
@@ -135,4 +111,46 @@ Node_Registers I2C::GetLastChange()
     Node_Registers returnVal = lastRecieved;
     lastRecieved = Node_Registers::NR_None;
     return returnVal;
+}
+
+typedef struct Address
+{
+    int address;
+    bool available;
+} Address;
+
+Address addresses[MAX_I2C_ADDRESSES];
+
+void InitializeAddresses()
+{
+    for (int i = 0; i < MAX_I2C_ADDRESSES; i++)
+    {
+        addresses[i].address = i;
+        addresses[i].available = true;
+    }
+}
+
+int GetAvailableAddress()
+{
+    for (int i = 0; i < MAX_I2C_ADDRESSES; i++)
+    {
+        if (addresses[i].available)
+        {
+            addresses[i].available = false;
+            return addresses[i].address;
+        }
+    }
+}
+
+bool ReleaseAddress(int address)
+{
+    for (int i = 0; i < MAX_I2C_ADDRESSES; i++)
+    {
+        if (addresses[i].address == address)
+        {
+            addresses[i].available = true;
+            return true;
+        }
+    }
+    return false;
 }
