150afd5f70213b8e8faa77ba30346f01c5066968
coen hezemans <i481948@fhict.nl>
2023-05-30 14:06:36 +0200

    set up main

--- Source/Node/src/main.cpp
+++ Source/Node/src/main.cpp
@@ -5,26 +5,24 @@
 #include "MFRC522Reader.hpp"
 
 #define CARDSTRINGLENGTH 30
-// I2CCommunication* comToHub;
+I2CCommunication* comToHub;
 MFRC522Reader* nfcReader;
 
-char currentCard[CARDSTRINGLENGTH] = "";
+char currentCard[CARDSTRINGLENGTH] = "\0";
 
 void setup()
 {
     Serial.begin(9600);
-    // comToHub = new I2CCommunication();
+    comToHub = new I2CCommunication();
     nfcReader = new MFRC522Reader();
 }
 
 void loop()
 {
-    // static char currentCard[CARDSTRINGLENGTH] = "12345123451234512345123451234";
     bool status = nfcReader->CheckForCard();
-    // delay(1000);
-    // comToHub->SendNewItemToHub(currentCard, CARDSTRINGLENGTH);
     if (status)
     {
+
         char card[CARDSTRINGLENGTH];
         nfcReader->ReadCard(card, CARDSTRINGLENGTH);
 
@@ -32,13 +30,13 @@ void loop()
         {
             strcpy(currentCard, card);
             Serial.println(currentCard);
-            // comToHub->SendNewItemToHub(card, CARDSTRINGLENGTH);
+            comToHub->SendNewItemToHub(card, CARDSTRINGLENGTH);
         }
     }
     else if (currentCard[0] != '\0')
     {
         Serial.println("Card removed");
         currentCard[0] = '\0';
-        // comToHub->SendNewItemToHub(currentCard, CARDSTRINGLENGTH);
+        comToHub->SendNewItemToHub(currentCard, CARDSTRINGLENGTH);
     }
 }
