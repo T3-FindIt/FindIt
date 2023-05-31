5b1318c67fd27e7bb069483fcf74d5f7b0ff8b23
Bas Mentjot <bas.mentjot@vanderlande.com>
2023-05-31 15:13:09 +0200

    JSonBuilder review

--- Source/Master/lib/JsonBuilder/JsonBuilder.cpp
+++ Source/Master/lib/JsonBuilder/JsonBuilder.cpp
@@ -1,10 +1,14 @@
 #include "JsonBuilder.hpp"
 #include "json.hpp"
-using json = nlohmann::json;
+using json = nlohmann::json; //Please abstain from using this.
 
+//This is a C-style function. It should return the objects and throw an exception if anything VERY wrong
+//Otherwise you should just check the output to see if anything went wrong.
+//I think returning an std::map here would work perfectly. 
+//Or maybe just returning the json object??? Don't really see why not, as it seems to work like a map already.
 bool JsonBuilder::Deserialize(const std::string &Json, std::string keys[], std::string values[], const int size)
 {
-    if (keys == nullptr || values == nullptr)
+    if (keys == nullptr || values == nullptr) //Would not be necessary if not done C-Style.
     {
         return false;
     }
@@ -38,6 +42,9 @@ bool JsonBuilder::Deserialize(const std::string &Json, std::string keys[], std::
     return true;
 }
 
+//Same comments as above.
+//You seem to be translating these json objects into 2 arrays for no reason.
+//They are already bundled nicely as keyValuePairs. Just keep them that way. No reason to do it in any other way.
 bool JsonBuilder::Serialize(const std::string keys[], const std::string values[], const int size, std::string &output)
 {
     if (keys == nullptr || values == nullptr)
@@ -56,7 +63,8 @@ bool JsonBuilder::Serialize(const std::string keys[], const std::string values[]
         serializedJson[keys[i]] = values[i];
     }
     
-    output = serializedJson.dump(); // Convert to string
+    //Why not return this instead of an out variable?
+    output = serializedJson.dump(); // Convert to string 
 
     return true;
 }
--- Source/Master/lib/JsonBuilder/JsonBuilder.hpp
+++ Source/Master/lib/JsonBuilder/JsonBuilder.hpp
@@ -6,12 +6,17 @@
 class JsonBuilder
 {
 public:
-    JsonBuilder() = default;
+    JsonBuilder() = default; //Nice
+
+    //I love doxyen style comments
+    //However, if it isn't done everywhere in the code and/or updating them isn't a part of the Way of Working for a pr
+    //They tend to become hassles more than helpful fairly quickly.
+    //So I advice against them.
 
     /// @brief Deserializes a JSON string into the given keys and values
     /// @param Json The input JSON string
-    /// @param keys The keys to use
-    /// @param values The values to use
+    /// @param keys The keys to use //Especially when they're wrong. This is clearly an out variable.
+    /// @param values The values to use //Same here
     /// @param size The size of the keys and values arrays
     /// @return True if the deserialization was successful, false otherwise
     bool Deserialize(const std::string &Json, std::string keys[], std::string values[], const int size);
@@ -20,7 +25,7 @@ public:
     /// @param keys The keys to use
     /// @param values The values to use
     /// @param size The size of the keys and values arrays
-    /// @param output The output string
+    /// @param output The output string //Out variable
     /// @return True if the serialization was successful, false otherwise
     bool Serialize(const std::string keys[], const std::string values[], const int size, std::string &output);
 };
