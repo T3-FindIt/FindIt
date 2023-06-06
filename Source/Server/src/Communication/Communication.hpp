c95614d2fd5a9d54b807009804228c7884da06f4
Verlinde,Tom T.S <t.verlinde@student.fontys.nl>
2023-06-06 15:31:22 +0200

    made a start

--- Source/Server/src/Communication/Communication.hpp
+++ Source/Server/src/Communication/Communication.hpp
@@ -7,24 +7,49 @@
 #include <IMessage.hpp>
 #include <HeartBeat.hpp>
 #include <HeartbeatResponse.hpp>
+#include <NodeSignIn.hpp>
+#include <NodeSignInResponse.hpp>
 
-#define MAX_CLIENT_INACTIVITY_TIME 10000
+#include <chrono>
+
+constexpr uint64_t MAX_CLIENT_INACTIVITY_TIME = 10000;
 
 namespace FindIt
 {
 
+struct message_track_t
+{
+    std::chrono::time_point<std::chrono::system_clock> time = std::chrono::system_clock::now();
+    FindIt::MessageType type = FindIt::MessageType::INVALID;
+    std::shared_ptr<FindIt::IMessage> data = nullptr;
+};
+
+struct client_t
+{
+    uint64_t id;
+    std::chrono::time_point<std::chrono::system_clock> lastCommunication = std::chrono::system_clock::now();
+
+    message_track_t lastInMessage;
+    message_track_t lastOutMessage;
+};
+
 class Communication
 {
-private:
-    IClusterConnection& clusterConnection;
-    IProtocolParser& protocolParser;
-    bool isRunning = false;
 public:
     Communication(IClusterConnection& clusterConnection, IProtocolParser& protocolParser);
     ~Communication();
 
     void Run();
     void Stop();
+private:
+    IClusterConnection& clusterConnection;
+    IProtocolParser& protocolParser;
+    bool isRunning = false;
+    std::unordered_map<uint64_t, client_t> clients;
+
+    void onMessage(const uint64_t client, const std::string& message);
+    void onConnect(const uint64_t client);
+    void onDisconnect(const uint64_t client);
 };
 
 };
