03ba9b842aab9d9025af135b92e99bf8bbd2579d
Nathan Thus <99728206+NathanThus@users.noreply.github.com>
2023-06-13 10:35:34 +0200

    Removed Enum due to edge case

--- Source/Master/lib/Communication Abstraction/IMasterSlave.hpp
+++ Source/Master/lib/Communication Abstraction/IMasterSlave.hpp
@@ -1,17 +1,6 @@
 #ifndef IMASTERSLAVE_HPP
 #define IMASTERSLAVE_HPP
 
-// enum Node_Registers
-// {
-//     NR_None = 255,
-//     NR_RequestForm = 0,
-//     NR_Notification = 1, // | SOUND | VIBRATION | LED | RGB | 0 | 0 | 0 | 0 |
-//     NR_RGB = 2, // | R | G | B | 0 | 0 | 0 | 0 | 0 |
-//     NR_Item = 3, // Points to a char[] of size 48.
-//     NR_Active = 4, // 0 = inactive, 1 = active
-//     NR_Error = 5,
-// };
-
 class IMasterSlave
 {
     public:
