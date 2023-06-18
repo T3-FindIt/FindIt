b70feec1a1f365ced5300d669cf6c9946a6e295a
Verlinde,Tom T.S <t.verlinde@student.fontys.nl>
2023-06-18 21:52:43 +0200

    Integrated everything in main

--- Source/Server/src/main.cpp
+++ Source/Server/src/main.cpp
@@ -1,17 +1,27 @@
-// #include <nlohmann/json.hpp>
 #include <iostream>
 #include <thread>
 #include <vector>
-#include <string>
-#include <PlainFileDatabase.hpp>
-#include <filesystem>
-#include <fstream>
+#include <memory>
 
+#include "Communication.hpp"
+#include "TCPConnection.hpp"
+#include "JSONProtocolParser.hpp"
+#include "ItemType.hpp"
+#include "PlainFileDatabase.hpp"
+#include "IMessage.hpp"
+#include "MessageQueue.hpp"
+#include "UserInterface.hpp"
 
 #include "./UserInterface/UserInterface.hpp"
 
 int main()
 {
+    // Set cluster coms to a TCP connection on port 54000
+    FindIt::Server clusterConnection(54000);
+
+    // Set protocol parser to JSON
+    FindIt::JSONProtocolParser protocolParser;
+
     // Set up database
     FindIt::PlainFileDatabase database("./Data/Database.db");
 
@@ -21,29 +31,18 @@ int main()
 
     // Set up communication and start a new thread for it
     FindIt::Communication communication(clusterConnection, protocolParser, database, UIToCommunication, CommunicationToUI);
-    std::jthread communicationThread2(&FindIt::Communication::Run, &communication);
+    std::jthread communicationThread(&FindIt::Communication::Run, &communication);
 
-    FindIt::UserInterface *UI = new FindIt::UserInterface(std::bind_front(&FindIt::PlainFileDatabase::GetAllObjects, &database)
-                                                            , std::bind_front(&FindIt::PlainFileDatabase::Add, &database)
-                                                            , msgq1
-                                                            , msgq2);
+    // Set up UI and start a new thread for it
+    FindIt::UserInterface UI(std::bind_front(&FindIt::PlainFileDatabase::GetAllObjects, &database)
+                                                , std::bind_front(&FindIt::PlainFileDatabase::Add, &database)
+                                                , CommunicationToUI
+                                                , UIToCommunication);
+    std::jthread uiThread(&FindIt::UserInterface::Run, &UI);
 
-    std::jthread t1(&FindIt::UserInterface::Run, UI);
-    // communication->Stop();
-    // communicationThread.join();
-
-    database.Add(FindIt::ItemType("TEST_JOHN"));
-    database.Add(FindIt::ItemType("TEST_TEST"));
-    database.Add(FindIt::ItemType("TEST_TEST"));
-    database.Add(FindIt::ItemType("TEST_BARB"));
-    database.Add(FindIt::ItemType("TEST_JOHNS"));
-    database.SearchIfPresent(FindIt::ItemType("TEST_TEST"));
-
-    std::vector<FindIt::ItemType> objects = database.GetAllObjects();
-
-    database.Remove(FindIt::ItemType("TEST_TEST"));
+    // Make sure we exit gracefully
+    uiThread.join();
     communication.Stop();
-    t1.join();
-
-    delete UI;
+    communicationThread.join();
+    return 0;
 }
